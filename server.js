7d4058fa007e89d85e7f9a962ec8b7ccaae99bad
srishcodes <srishtisaini4444@gmail.com>
2026-07-02 14:12:03 +0530

    Setup Express server and configure routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "login.html"));
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "register.html"));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
