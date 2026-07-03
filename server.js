3eb4a83dec83a581bf9b1aded0f958923ac33b14
srishcodes <srishtisaini4444@gmail.com>
2026-07-03 10:41:07 +0530

    Configure Express app for Vercel

--- server.js
+++ server.js
@@ -295,6 +295,10 @@ if (entry.userId !== currentUserId) {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
